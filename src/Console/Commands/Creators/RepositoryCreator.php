6154aa94ae097bd1083cdc0e161dae656de98be1
Christian Dominguez <Christian@redbonzai.com>
2019-07-27 08:52:38 -0500

    Create model using the settings in the repositories.php config file.  The current default namespace for models in the repositories.php was set to App\Entities.  Also, the path was set to 'app' . DIRECTORY_SEPARATOR . 'Entities'.  The RepositoryCreator.php file now reads that. Ideally, I think the generator config should read something like this :    'generator'=>[         'basePath'=>app()->path(),         'rootNamespace'=>'App\\',         'paths'=>[             'models'=>'Models',             'repositories'=>'Repositories\\Eloquent',             'interfaces'=>'Contracts\\Repositories',             'transformers'=>'Transformers',             'presenters'=>'Presenters'             'validators'   => 'Validators',             'controllers'  => 'Http/Controllers',             'provider'     => 'RepositoryServiceProvider',             'criteria'     => 'Criteria',         ]     ]
    This pattern I got from this repository:  https://github.com/andersao/l5-repository  BTW, I think they have abandoned this repo as well.  It has a lot of very useful features.

--- src/Console/Commands/Creators/RepositoryCreator.php
+++ src/Console/Commands/Creators/RepositoryCreator.php
@@ -1,99 +1,251 @@
 <?php
 
-namespace Dugajean\Repositories\Console\Commands\Creators;
+    namespace Dugajean\Repositories\Console\Commands\Creators;
 
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Artisan;
-use Doctrine\Common\Inflector\Inflector;
+    use Illuminate\Filesystem\Filesystem;
+    use Illuminate\Support\Facades\Config;
+    use Illuminate\Support\Facades\Artisan;
+    use Illuminate\Database\Eloquent\Model;
+    use Doctrine\Common\Inflector\Inflector;
+    use Dugajean\Repositories\Eloquent\Repository;
 
-class RepositoryCreator extends BaseCreator
-{
-    /**
-     * Get the repository name.
-     *
-     * @return mixed|string
-     */
-    public function getName()
+    class RepositoryCreator
     {
-        $repositoryName = parent::getName();
+        /**
+         * @var Filesystem
+         */
+        protected $files;
 
-        if (strpos($repositoryName, 'Repository') === false) {
-            $repositoryName .= 'Repository';
+        /**
+         * @var Repository
+         */
+        protected $repository;
+
+        /**
+         * @var Model
+         */
+        protected $model;
+
+        /**
+         * @param Filesystem $files
+         */
+        public function __construct(Filesystem $files)
+        {
+            $this->files = $files;
         }
 
-        // Return repository name.
-        return $repositoryName;
-    }
+        /**
+         * @return mixed
+         */
+        public function getRepository()
+        {
+            return $this->repository;
+        }
 
-    /**
-     * Get the populate data.
-     *
-     * @return array
-     */
-    protected function getPopulateData()
-    {
-        $repositoryNamespace = Config::get('repositories.repository_namespace');
-        $repositoryClass = $this->getName();
-        $modelPath = Config::get('repositories.model_namespace');
-        $modelName = $this->getModelName();
+        /**
+         * @param mixed $repository
+         */
+        public function setRepository($repository)
+        {
+            $this->repository = $repository;
+        }
 
-        $populateData = [
-            'repository_namespace' => $repositoryNamespace,
-            'repository_class' => $repositoryClass,
-            'model_path' => $modelPath,
-            'model_name' => $modelName,
-        ];
+        /**
+         * @return Model
+         */
+        public function getModel()
+        {
+            return $this->model;
+        }
 
-        return $populateData;
-    }
+        /**
+         * @param Model $model
+         */
+        public function setModel($model)
+        {
+            $this->model = $model;
+        }
 
-    /**
-     * Creates the class.
-     *
-     * @return bool|int
-     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
-     */
-    protected function createClass()
-    {
-        $model = Config::get('repositories.model_namespace') . '\\' . $this->name;
+        /**
+         * Create the repository.
+         *
+         * @param Repository $repository
+         * @param Model      $model
+         *
+         * @return int
+         * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+         */
+        public function create($repository, $model)
+        {
+            $this->setRepository($repository);
+            $this->setModel($model);
+            $this->createDirectory();
 
-        if (!class_exists($model)) {
-            if ($this->command->confirm("Do you want to create a {$model} model?")) {
-                Artisan::call('make:model', ['name' => $this->name]);
-            } else {
-                throw new \RuntimeException("Could not create repository: Model {$model} does not exist.");
+            return $this->createClass();
+        }
+
+        /**
+         * Create the necessary directory.
+         */
+        protected function createDirectory()
+        {
+            $directory = $this->getDirectory();
+
+            if (!$this->files->isDirectory($directory)) {
+                $this->files->makeDirectory($directory, 0755, true);
             }
         }
 
-        if ($this->files->exists($this->getPath())) {
-            throw new \RuntimeException("The repository with the name '{$this->getName()}' already exists.");
+        /**
+         * Get the repository directory.
+         *
+         * @return mixed
+         */
+        protected function getDirectory()
+        {
+            return Config::get('repositories.repository_path');
         }
 
-        return $this->files->put($this->getPath(), $this->populateStub());
+        /**
+         * Get the repository name.
+         *
+         * @return mixed|string
+         */
+        protected function getRepositoryName()
+        {
+            $repositoryName = $this->getRepository();
+
+            if (!strpos($repositoryName, 'Repository') !== false) {
+                $repositoryName .= 'Repository';
+            }
+
+            // Return repository name.
+            return $repositoryName;
+        }
+
+        /**
+         * Get the model name.
+         *
+         * @return string
+         */
+        protected function getModelName()
+        {
+            $model = $this->getModel();
+
+            if (isset($model) && !empty($model)) {
+                $modelName = $model;
+            } else {
+                $modelName = Inflector::singularize($this->stripRepositoryName());
+            }
+
+            return $modelName;
+        }
+
+        /**
+         * Get the stripped repository name.
+         *
+         * @return string
+         */
+        protected function stripRepositoryName()
+        {
+            $repository = strtolower($this->getRepository());
+            $stripped = str_replace("repository", "", $repository);
+            $result = ucfirst($stripped);
+
+            return $result;
+        }
+
+        /**
+         * Get the populate data.
+         *
+         * @return array
+         */
+        protected function getPopulateData()
+        {
+            $repositoryNamespace = Config::get('repositories.repository_namespace');
+            $repositoryClass = $this->getRepositoryName();
+            $modelPath = Config::get('repositories.model_path');
+            $modelName = $this->getModelName();
+            $modelNamespace = Config::get('repositories.model_namespace');
+
+            $populateData = [
+                'repository_namespace' => $repositoryNamespace,
+                'repository_class' => $repositoryClass,
+                'model_path' => $modelPath,
+                'model_namespace' => $modelNamespace,
+                'model_name' => $modelName,
+            ];
+
+            return $populateData;
+        }
+
+        /**
+         * Get the path.
+         *
+         * @return string
+         */
+        protected function getPath()
+        {
+            return $this->getDirectory() . DIRECTORY_SEPARATOR . $this->getRepositoryName() . '.php';
+        }
+
+        /**
+         * Get the stub.
+         *
+         * @return string
+         * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+         */
+        protected function getStub()
+        {
+            return $this->files->get($this->getStubPath() . "repository.stub");
+        }
+
+        /**
+         * Get the stub path.
+         *
+         * @return string
+         */
+        protected function getStubPath()
+        {
+            return __DIR__ . '/../../../../resources/stubs/';
+        }
+
+        /**
+         * Populate the stub.
+         *
+         * @return mixed
+         * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+         */
+        protected function populateStub()
+        {
+            $populate_data = $this->getPopulateData();
+            $stub = $this->getStub();
+
+            foreach ($populate_data as $key => $value) {
+                $stub = str_replace($key, $value, $stub);
+            }
+
+            return $stub;
+        }
+
+        /**
+         * Creates the class.
+         *
+         * @return bool|int
+         * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+         */
+        protected function createClass()
+        {
+            $model = explode('\\', Config::get('repositories.model_namespace'))[1] . '\\' . $this->repository;
+
+            if (!class_exists($model)) {
+                Artisan::call('make:model', ['name' => $model]);
+            }
+
+            if ($this->files->exists($this->getPath())) {
+                throw new \RuntimeException("The repository with the name '$this->repository' already exists.");
+            }
+
+            return $this->files->put($this->getPath(), $this->populateStub());
+        }
     }
-
-    /**
-     * Get the model name.
-     *
-     * @return string
-     */
-    private function getModelName()
-    {
-        $model = $this->getModel();
-
-        return isset($model) && !empty($model) ? $model : Inflector::singularize($this->stripRepositoryName());
-    }
-
-    /**
-     * Get the stripped repository name.
-     *
-     * @return string
-     */
-    private function stripRepositoryName()
-    {
-        $stripped = str_ireplace('repository', '', $this->getName());
-        $result = ucfirst($stripped);
-
-        return $result;
-    }
-}
